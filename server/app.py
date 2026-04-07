cb81326ac8dd76f1297ffa45d1d878ad755ee244
Pranav Mudgil <pranavmudgil333@gmail.com>
2026-04-07 14:31:50 +0530

    Added necessary arguments to api creation call

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from openenv.core.env_server import create_fastapi_app
+from models import HospitalAction, HospitalObservation
 from server.environment import HospitalEnvironment
 
-app = create_fastapi_app(HospitalEnvironment)
+app = create_fastapi_app(HospitalEnvironment, HospitalAction, HospitalObservation)
